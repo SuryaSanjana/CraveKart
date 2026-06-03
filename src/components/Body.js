c0a650a0895f7840d8d500477b199c73269579cb
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2026-06-03 23:16:45 +0530

    Fetching Data from API using hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,34 +1,51 @@
 import SearchBar from "./SearchBar";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";  //named import
+
+import { useState, useEffect } from "react";  //named import
 
 
-const Body = () =>{
+const Body = () => {
     //useState hook to manage the state of top rated restaurants
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);  //initial state is an empty array
     //function to handle the click event of top rated button
     const handleTopRatedClick = () => {
         const filteredList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating >= 4.0);
         setListOfRestaurants(filteredList);  //setter function to update the state of top rated restaurants to trigger a re-render of the component with the filtered list of top rated restaurants
     }
-return (
-    
+
+    //fetching data from API by bypassing the CORS policy
+    useEffect(() => {
+        fetchData();
+
+    }, []);
+    const fetchData = async () => {
+        const data = await fetch("https://dummyjson.com/products",
+        );
+        const jsonData = await data.json();
+        setListOfRestaurants(jsonData.products);
+        console.log(jsonData.products);
+    }
+
+    if(listOfRestaurants.length === 0) {
+        return <h1>Loading...</h1>
+    }
+    return (
+
         <div id="body" className="body">
             <SearchBar />
 
             <div id="top-rated" className="filter-btn">
-                <button className="top-rated-btn"  onClick={handleTopRatedClick}>
+                <button className="top-rated-btn" onClick={handleTopRatedClick}>
                     Top Rated Restaurants
                 </button>
-            </div>  
+            </div>
             <div className="restaurant-container">
-                {(listOfRestaurants.map((restaurant) => {
-                    return <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                {(listOfRestaurants.map((product) => {
+                    return <RestaurantCard key={product?.id} resData={product} />
                 }))}
             </div>
-    </div>
-);
+        </div>
+    );
 }
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,14 +2,13 @@ import { CDN_URL } from "../utils/constants";   //named import
 
 const RestaurantCard = (props) => {
     const { resData } = props;
-    
+
     return (
 
         <div className="restaurant-card">
-            <img className="restaurant-image" src={CDN_URL} alt={resData.info.name} />
-            <h3 className="restaurant-name">{resData.info.name}</h3>
-            <p className="restaurant-cuisine">{resData.info.cuisines.join(", ")}</p>
-            <p className="restaurant-rating">Rating: {resData.info.avgRating}</p>
+            <img className="restaurant-image" src={CDN_URL} alt={resData.title} />
+            <h3 className="restaurant-name">{resData.title}</h3>
+            <p className="restaurant-rating">Rating: {resData.rating}</p>
         </div>
     );
 }
