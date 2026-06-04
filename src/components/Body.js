bbf36d4157d30dadef10d26cfa83094fa2b5d084
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2026-06-05 00:56:15 +0530

    implented search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,10 +8,14 @@ import { useState, useEffect } from "react";  //named import
 const Body = () => {
     //useState hook to manage the state of top rated restaurants
     const [listOfRestaurants, setListOfRestaurants] = useState([]);  //initial state is an empty array
+    const [filter, setFilter] = useState(false);  //state to manage the filter button click event
+    const [filteredRestaurants, setFilteredRestaurants] = useState(listOfRestaurants);  //state to manage the filtered list of restaurants based on the search text
+
     //function to handle the click event of top rated button
     const handleTopRatedClick = () => {
-        const filteredList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating >= 4.0);
-        setListOfRestaurants(filteredList);  //setter function to update the state of top rated restaurants to trigger a re-render of the component with the filtered list of top rated restaurants
+        const filteredList = filteredRestaurants.filter(restaurant => restaurant.rating >= 4.0);
+        setFilteredRestaurants(filteredList);  //setter function to update the state of filtered restaurants to trigger a re-render of the component with the filtered list of top rated restaurants
+        setFilter(true);  //update the filter state to true to indicate that the filter is applied
     }
 
     //fetching data from API by bypassing the CORS policy
@@ -19,32 +23,41 @@ const Body = () => {
         fetchData();
 
     }, []);
+
     const fetchData = async () => {
         const data = await fetch("https://dummyjson.com/products",
         );
         const jsonData = await data.json();
         setListOfRestaurants(jsonData.products);
+        setFilteredRestaurants(jsonData.products);  // Initialize filtered restaurants with all restaurants
+        setFilter(false);  //update the filter state to false to indicate that the filter is removed
         console.log(jsonData.products);
     }
 
     //conditional rendering to show the shimmer UI while the data is being fetched and to show the list of restaurants once the data is fetched
-    if(listOfRestaurants.length === 0) {
+    if (listOfRestaurants.length === 0) {
         return <ShimmerUI />
     }
     return (
 
         <div id="body" className="body">
-            <SearchBar />
+            <SearchBar listOfRestaurants={listOfRestaurants} filteredRestaurants={filteredRestaurants} setFilteredRestaurants={setFilteredRestaurants} />
 
             <div id="top-rated" className="filter-btn">
-                <button className="top-rated-btn" onClick={handleTopRatedClick}>
-                    Top Rated Restaurants
+                <button className="top-rated-btn" onClick={handleTopRatedClick} disabled={filter}>
+                    <i className="fas fa-star"></i> Top Rated Restaurants
+                </button>
+                {/*TODO:  Need a better refinement of remove filter */}
+                <button className="remove-filter-btn" onClick={() =>{setFilteredRestaurants(listOfRestaurants); setFilter(false);}} disabled={!filter}>
+                    <i className="fas fa-filter"></i> Remove Filter
                 </button>
             </div>
             <div className="restaurant-container">
-                {(listOfRestaurants.map((product) => {
-                    return <RestaurantCard key={product?.id} resData={product} />
-                }))}
+                {
+                    (filteredRestaurants.length > 0)
+                    ?   filteredRestaurants.map((product) => { return <RestaurantCard key={product?.id} resData={product} /> })
+                    :   <p>No restaurants found.</p>
+                }
             </div>
         </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,25 @@
-const SearchBar = () =>
-(
-    <div className="Search">
-        <div>
-            <input type="text" placeholder="Search for restaurants or dishes..." />
+import { useState } from "react";  //named import
+
+const SearchBar = (props) => {
+
+    const { listOfRestaurants, filteredRestaurants, setFilteredRestaurants } = props;
+    const [searchText, setSearchText] = useState("");
+
+    const filterRestaurants = () => {
+        const filteredList = listOfRestaurants.filter(restaurant => restaurant.title.toLowerCase().includes(searchText.toLowerCase()));
+        setFilteredRestaurants(filteredList);  //setter function to update the state of list of restaurants to trigger a re-render of the component with the filtered list of restaurants 
+
+    }
+    return (
+        <div className="search">
+            <div>
+                <input type="text" placeholder="Search for restaurants or dishes..." value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+            </div>
+            <div>
+                <button onClick={filterRestaurants}>Search</button>
+            </div>
         </div>
-        <div>
-            <button>Search</button>
-        </div>
-    </div>
-);
+    );
+}
 
 export default SearchBar;
