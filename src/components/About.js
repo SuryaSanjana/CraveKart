8757e55d725cbbfde316f8fc3c4a204b45a1be21
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2026-06-10 17:27:15 +0530

    Implement About page using Class Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import UserClass from './UserClass';
 
-const About = () => {
-  return (
-    <div>About</div>
-  )
+
+class About extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>About Us</h1>
+                <p> This is the about us page of our food delivery app. </p>
+                <UserClass />
+            </div>
+        )
+    }
 }
-
 export default About
---
+++ src/components/UserClass.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class UserClass extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+    return (
+        <div>
+            <h1>Class Component</h1>
+            <h2> This is a class component. </h2>
+        </div>
+    )
+    }
+}
+
+export default UserClass;
